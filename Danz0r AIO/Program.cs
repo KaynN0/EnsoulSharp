34628254b26bbc5b0b18e03f239f0823dd78d880
Danz0r <52358334+KaynN0@users.noreply.github.com>
2019-07-09 02:28:07 -0300

    Add files via upload

---
+++ Danz0r AIO/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using EnsoulSharp;
+using EnsoulSharp.SDK;
+using Danz0r_AIO.Champions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Danz0r_AIO
+{
+    
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += On_LoadGame;
+        }
+        private static void On_LoadGame()
+        {
+
+            if (ObjectManager.Player.CharacterName == ObjectManager.Player.CharacterName)
+            {
+                Morgana.OnLoad();
+                Chat.PrintChat("<font color=\"#e02db4\">[</font><font color=\"#e67d1c\">Morgana</font><font color=\"#e02db4\">]</font><font color=\"#e02db4\"> - <u>The Best Morgan Script</u>  </font>- Danz0r");
+            }
+        }
+    }
+}
